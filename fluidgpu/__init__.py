333418924713aa347205f1d561b650776ae32caa
pawel002 <paweljarosz002@gmail.com>
2025-12-10 14:59:20 +0100

    remove unused vector add function

--- fluidgpu/__init__.py
+++ fluidgpu/__init__.py
@@ -1,17 +1,6 @@
 import numpy as np
 from fluidgpu import _wrapper as _cwrapper
 
-def vector_add(A: np.ndarray, B: np.ndarray) -> np.ndarray:
-    assert isinstance(A, np.ndarray) and isinstance(B, np.ndarray), "Expected numpy arrays"
-    assert A.shape == B.shape, "Expected arrays to have the same shape"
-    assert (dim := len(A.shape)) == 1, f"Expected 1-D arrays, got {dim}-D arrays"
-
-    n = A.shape[0]
-    return _cwrapper.add_vectors(
-        A.astype(np.float32),
-        B.astype(np.float32),
-        n,
-    )
 
 def solver_steps(
     u: np.ndarray,
--- fluidgpu/_wrapper.cu
+++ fluidgpu/_wrapper.cu
@@ -3,63 +3,7 @@
 #include <Python.h>
 #include <numpy/arrayobject.h>
 
-#include "vector_add.cuh"
 #include "burgers_solver.cuh"
-
-static PyObject* add_vectors(PyObject* self, PyObject* args)
-{
-    PyObject *A_obj = NULL;
-    PyObject *B_obj = NULL;
-    Py_ssize_t n_py = 0;
-
-    // parse arguments
-    if (!PyArg_ParseTuple(args, "OOn", &A_obj, &B_obj, &n_py)) {
-        return NULL; 
-    }
-
-    npy_intp n = (npy_intp)n_py;
-
-    // convert to right pointers
-    PyArrayObject *A_arr = (PyArrayObject*) PyArray_FROM_OTF(A_obj, NPY_FLOAT32, NPY_ARRAY_IN_ARRAY);
-    PyArrayObject *B_arr = (PyArrayObject*) PyArray_FROM_OTF(B_obj, NPY_FLOAT32, NPY_ARRAY_IN_ARRAY);
-
-    // check for errors
-    if (A_arr == NULL || B_arr == NULL) {
-        Py_XDECREF(A_arr);
-        Py_XDECREF(B_arr);
-        return NULL;
-    }
-
-    // allocate output
-    npy_intp dims[1] = { n };
-    PyArrayObject *C_arr = (PyArrayObject*) PyArray_SimpleNew(1, dims, NPY_FLOAT);
-    if (C_arr == NULL) {
-        Py_DECREF(A_arr);
-        Py_DECREF(B_arr);
-        return NULL;
-    }
-    
-    // raw pointers
-    float *h_A = (float*) PyArray_DATA(A_arr);
-    float *h_B = (float*) PyArray_DATA(B_arr);
-    float *h_C = (float*) PyArray_DATA(C_arr);
-
-    // addition on GPU
-    int status = vector_add_cuda(h_A, h_B, h_C, n);
-
-    // cleanup
-    Py_DECREF(A_arr);
-    Py_DECREF(B_arr);
-
-    // check for error
-    if (status != 0) {
-        Py_DECREF(C_arr);
-        PyErr_SetString(PyExc_RuntimeError, "CUDA vector_add failed");
-        return NULL;
-    }
-
-    return (PyObject*) C_arr;
-}
 
 static PyObject* burgers_solver(PyObject* self, PyObject* args)
 {
@@ -114,8 +58,7 @@ static PyObject* burgers_solver(PyObject* self, PyObject* args)
 
 // method table
 static PyMethodDef methods[] = {
-    { "add_vectors",  (PyCFunction) add_vectors,    METH_VARARGS, "Elementwise addition of two 1D float32 arrays: C = A + B.\n" },
-    { "solver_steps", (PyCFunction) burgers_solver, METH_VARARGS, "Solve 2D Burgers equation steps."},
+    { "solver_steps", (PyCFunction) burgers_solver, METH_VARARGS, "Solve 2D Burgers equation steps"},
     { NULL, NULL, 0, NULL }
 };
 
